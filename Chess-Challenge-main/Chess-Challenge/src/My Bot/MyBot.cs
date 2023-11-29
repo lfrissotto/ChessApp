cd60cfbefc20e2f5abafb987e424068ec558e33b
lfrissotto <lfrissotto@gmail.com>
2023-11-28 21:08:11 -0300

    Last Version
    Last Updated Version

---
+++ Chess-Challenge-main/Chess-Challenge/src/My Bot/MyBot.cs
@@ -0,0 +1,11 @@
+﻿using ChessChallenge.API;
+
+public class MyBot : IChessBot
+{
+    
+    public Move Think(Board board, Timer timer)
+    {
+        Move[] moves = board.GetLegalMoves();
+        return moves[0];
+    }
+}
